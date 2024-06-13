f2b440bd8fafb2a1ccabf47f0e2409558df9d119
Jeremias Ahumada <jeremias.alonso77@gmail.com>
2024-06-13 18:47:05 -0400

    primer-upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
+import RequireAuth from "./components/RequireAuth";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <RequireAuth>
+                            <Home />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+            </Routes>
+        </>
+    );
+};
+
+export default App;
