402fe07697a3fd298babe193a7e28a902d7668c9
Jeremias Ahumada <jeremias.alonso77@gmail.com>
2024-06-19 19:58:24 -0400

    refactoring form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,86 +3,61 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
 
 import { useForm } from "react-hook-form";
+import FormError from "../components/FormError";
+import FormInput from "../components/FormInput";
+import { erroresFirebase } from "../utils/erroresFirebase";
+import { formValidate } from "../utils/formValidate";
 
 const Login = () => {
+    const { loginUser } = useContext(UserContext);
     const navegate = useNavigate();
-    const { registerUser } = useContext(UserContext);
+    const { required, patternEmail, minLength, validateTrim } = formValidate();
 
     const {
         register,
         handleSubmit,
         formState: { errors },
         setError,
-    } = useForm({
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+    } = useForm();
 
     const onSubmit = async ({ email, password }) => {
         try {
-            await registerUser(email, password);
-            console.log("Usuario creado");
+            await loginUser(email, password);
             navegate("/");
         } catch (error) {
             console.log(error.code);
-            switch (error.code) {
-                case "auth/email-already-in-use":
-                    setError("email", {
-                        message: "Usuario ya registrado",
-                    });
-                    break;
-                case "auth/invalid-email":
-                    setError("email", {
-                        message: "Formato email no válido",
-                    });
-                    break;
-                default:
-                    console.log("Ocurrio un error en el server");
-            }
+            setError("firebase", {
+                message: erroresFirebase(error.code),
+            });
         }
     };
 
     return (
         <>
             <h1>Login</h1>
+            <FormError error={errors.firebase} />
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input
+                <FormInput
                     type="email"
                     placeholder="Ingrese email"
                     {...register("email", {
-                        required: {
-                            value: true,
-                            message: "campo obligatorio",
-                        },
-                        pattern: {
-                            value: /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/,
-                            message: "formato de email incorrecto",
-                        },
+                        required,
+                        pattern: patternEmail,
                     })}
-                />
-                {errors.email && <p>{errors.email.message}</p>}
-                <input
+                >
+                    <FormError error={errors.email} />
+                </FormInput>
+
+                <FormInput
                     type="password"
                     placeholder="Ingrese Password"
                     {...register("password", {
-                        setvalueas: (v) => v.trim(),
-                        minLength: {
-                            value: 6,
-                            message: "Mínimo 6 carácteres",
-                        },
-                        validate: {
-                            trim: (v) => {
-                                if (!v.trim()) {
-                                    return "Porfavor escriba una contraseña";
-                                }
-                                return true;
-                            },
-                        },
+                        minLength,
+                        validate: validateTrim,
                     })}
-                />
-                {errors.password && <p>{errors.password.message}</p>}
+                >
+                    <FormError error={errors.password} />
+                </FormInput>
 
                 <button type="submit">Login</button>
             </form>
---
+++ src/utils/erroresFirebase.js
@@ -0,0 +1,32 @@
+export const erroresFirebase = (error) => {
+    switch (error.code) {
+        case "auth/email-already-in-use":
+            return { code: "email", message: "Usuario ya registrado" };
+
+        case "auth/invalid-email":
+            return { code: "email", message: "Formato email no válido" };
+
+        case "auth/invalid-email-verified":
+            return { code: "email", message: "El email no está verificado" };
+
+        case "auth/invalid-password":
+            return {
+                code: "password",
+                message: "Contraseña mínimo 6 carácteres",
+            };
+        case "auth/invalid-login-credentials":
+            return {
+                code: "email",
+                message: "Revise si el correo o contraseña están bien escritos",
+            };
+
+        case "auth/user-not-found":
+            return { code: "email", message: "Usuario no registrado" };
+
+        case "auth/wrong-password":
+            return { code: "password", message: "Contraseña incorrecta" };
+
+        default:
+            return { code: "email", message: "Error, intentelo más tarde" };
+    }
+};
